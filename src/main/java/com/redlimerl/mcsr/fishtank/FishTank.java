bc0f674ff2183a50aca68e50b3562cf781a70a25
RedLime <redlimerl@gmail.com>
2022-02-16 11:13:46 +0900

    Initial commit

---
+++ src/main/java/com/redlimerl/mcsr/fishtank/FishTank.java
@@ -0,0 +1,59 @@
+package com.redlimerl.mcsr.fishtank;
+
+import net.fabricmc.api.ClientModInitializer;
+import net.minecraft.block.AbstractGlassBlock;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.entity.passive.FishEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.world.World;
+
+public class FishTank implements ClientModInitializer {
+
+    @Override
+    public void onInitializeClient() {
+    }
+
+    public static boolean checkFishTank(World world, FishEntity fish) {
+        BlockPos pos = fish.getBlockPos();
+
+        for (int x = 0; x < 3; x++) {
+            for (int z = 0; z < 3; z++) {
+                for (int y = 0; y < 2; y++) {
+                    BlockState blockState = world.getBlockState(pos.add(x - 1, y - 1, z - 1));
+                    if (x == 1 && z == 1 && y == 1 && blockState.getBlock() == Blocks.WATER) {
+                        continue;
+                    }
+                    if (!(blockState.getBlock() instanceof AbstractGlassBlock)) {
+                        return false;
+                    }
+                }
+            }
+        }
+
+        return checkFishIsSafe(world, fish);
+    }
+
+    private static boolean checkFishIsSafe(World world, FishEntity fish) {
+        BlockPos fishPos = fish.getBlockPos();
+        BlockState fishTankRoof = world.getBlockState(fishPos.add(0, 1, 0));
+
+        boolean isLavaSafe = true;
+        if (!fishTankRoof.getMaterial().isSolid()) {
+            for (int x = 0; x < 7; x++) {
+                for (int y = 0; y < 5; y++) {
+                    for (int z = 0; z < 7; z++) {
+                        BlockPos blockPos = fishPos.add(x - 4, y, z - 4);
+                        BlockState blockState = world.getBlockState(blockPos);
+                        if (blockState.getBlock() == Blocks.LAVA) {
+                            isLavaSafe = false;
+                            break;
+                        }
+                    }
+                }
+            }
+        }
+
+        return isLavaSafe;
+    }
+}
---
+++ src/main/java/com/redlimerl/mcsr/fishtank/mixins/ClientWorldMixin.java
@@ -0,0 +1,67 @@
+package com.redlimerl.mcsr.fishtank.mixins;
+
+import com.redlimerl.mcsr.fishtank.FishTank;
+import com.redlimerl.speedrunigt.timer.InGameTimer;
+import com.redlimerl.speedrunigt.timer.RunCategory;
+import com.redlimerl.speedrunigt.timer.TimerStatus;
+import net.minecraft.block.BlockState;
+import net.minecraft.block.Blocks;
+import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.passive.FishEntity;
+import net.minecraft.util.math.BlockPos;
+import net.minecraft.util.math.Box;
+import net.minecraft.util.profiler.Profiler;
+import net.minecraft.util.registry.RegistryKey;
+import net.minecraft.world.MutableWorldProperties;
+import net.minecraft.world.World;
+import net.minecraft.world.dimension.DimensionType;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.List;
+import java.util.function.Supplier;
+
+@Mixin(ClientWorld.class)
+public abstract class ClientWorldMixin extends World {
+
+    protected ClientWorldMixin(MutableWorldProperties mutableWorldProperties, RegistryKey<World> registryKey, RegistryKey<DimensionType> registryKey2, DimensionType dimensionType, Supplier<Profiler> profiler, boolean bl, boolean bl2, long l) {
+        super(mutableWorldProperties, registryKey, registryKey2, dimensionType, profiler, bl, bl2, l);
+    }
+
+    private final int playerRange = 8;
+    private BlockPos lastWaterBlockPos = BlockPos.ORIGIN;
+
+    @Inject(method = "addEntity", at = @At("TAIL"))
+    public void onEntityUpdate(int id, Entity entity, CallbackInfo ci) {
+        if (entity instanceof FishEntity && lastWaterBlockPos != BlockPos.ORIGIN
+                && entity.squaredDistanceTo(lastWaterBlockPos.getX(), lastWaterBlockPos.getY(), lastWaterBlockPos.getZ()) < playerRange) {
+            InGameTimer timer = InGameTimer.getInstance();
+
+            if (timer.getCategory() == RunCategory.CUSTOM && timer.getStatus() != TimerStatus.NONE
+                    && FishTank.checkFishTank(this, (FishEntity) entity)) {
+                InGameTimer.complete();
+            }
+        }
+    }
+
+    @Inject(method = "updateListeners", at = @At("TAIL"))
+    public void onBlockUpdate(BlockPos pos, BlockState oldState, BlockState newState, int flags, CallbackInfo ci) {
+        InGameTimer timer = InGameTimer.getInstance();
+
+        if (timer.getCategory() == RunCategory.CUSTOM && timer.getStatus() != TimerStatus.NONE) {
+            if (flags == 11 && newState.getBlock() == Blocks.WATER) {
+                lastWaterBlockPos = pos;
+            }
+
+            List<FishEntity> fishEntityList = this.getEntities(FishEntity.class, new Box(pos).expand(playerRange * 4), LivingEntity::isAlive);
+            for (FishEntity fishEntity : fishEntityList) {
+                if (FishTank.checkFishTank(this, fishEntity)) InGameTimer.complete();
+            }
+        }
+    }
+
+}
